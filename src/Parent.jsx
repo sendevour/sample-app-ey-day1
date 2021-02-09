919863c52be154c12ddd9d78de533f1ca8a272ff
Vivekanand P V <vivekanandpv@outlook.com>
2021-02-09 14:53:37 +0530

    done

--- src/Parent.jsx
+++ src/Parent.jsx
@@ -1,39 +1,106 @@
 import React, { Component } from 'react';
-import Child from './Child';
 
 class Parent extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      email: 'user@domain.com',
+      username: '',
+      password: '',
+      validationErrors: {
+        username: null,
+        password: null,
+      },
     };
-    this.emailRef = React.createRef();
   }
 
-  submitHandler = () => {
-    const val = this.emailRef.current.value;
-
-    if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(val)) {
-      this.setState({ email: val });
+  submitHandler = (e) => {
+    e.preventDefault();
+    if (this.isFormValid()) {
+      const formData = {
+        username: this.state.username,
+        password: this.state.password,
+      };
+      console.log('Submitted', formData);
+    } else {
+      alert('Invalid form');
     }
+  };
+
+  isFormValid = () => {
+    return !(
+      this.state.validationErrors.username ||
+      this.state.validationErrors.password
+    );
+  };
+
+  changeHandler = (e) => {
+    switch (e.target.name) {
+      case 'username':
+        this.updateUsername(e.target.value);
+        return;
+      case 'password':
+        this.updatePassword(e.target.value);
+        return;
+      default:
+        return;
+    }
+  };
+
+  updateUsername = (username) => {
+    if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(username)) {
+      this.setState({ username });
+      this.setState({
+        validationErrors: {
+          username: null,
+        },
+      });
+    } else {
+      this.setState({
+        validationErrors: {
+          username: 'Not a valid email',
+        },
+      });
+    }
+  };
+
+  updatePassword = (password) => {
+    this.setState({ password });
   };
 
   render() {
     return (
       <>
         <div className='container p-3'>
-          <h2>Current: {this.state.email}</h2>
-          <input type='email' className='form-control' ref={this.emailRef} />
-          <button
-            className='btn btn-primary btn-sm mt-2'
-            onClick={this.submitHandler}
-          >
-            Submit
-          </button>
-
+          <h2>Login Form</h2>
           <hr />
 
-          <Child email={this.state.email} />
+          <form noValidate autoComplete='off' onSubmit={this.submitHandler}>
+            <div className='form-group'>
+              <label>Username</label>
+              <input
+                type='text'
+                className='form-control'
+                onChange={this.changeHandler}
+                name='username'
+              />
+              {this.state.validationErrors.username ? (
+                <small className='text-danger'>
+                  {this.state.validationErrors.username}
+                </small>
+              ) : null}
+            </div>
+            <div className='form-group'>
+              <label>Password</label>
+              <input
+                type='password'
+                className='form-control'
+                onChange={this.changeHandler}
+                name='password'
+              />
+            </div>
+            <button className='btn btn-primary'>Login</button>
+          </form>
         </div>
       </>
     );
