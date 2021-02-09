d5a285fc54fa6cd392f4206f454d0f1a2b1a6701
Vivekanand P V <vivekanandpv@outlook.com>
2021-02-09 11:14:14 +0530

    done

--- src/Parent.jsx
+++ src/Parent.jsx
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
-import App from './App';
+import Child from './Child';
 
 class Parent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: 'user@domain.com',
+    };
+    this.emailRef = React.createRef();
+  }
+
+  submitHandler = () => {
+    const val = this.emailRef.current.value;
+
+    if (/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(val)) {
+      this.setState({ email: val });
+    }
+  };
+
   render() {
-    console.log('Parent: render');
-    return <App />;
+    return (
+      <>
+        <div className='container p-3'>
+          <h2>Current: {this.state.email}</h2>
+          <input type='email' className='form-control' ref={this.emailRef} />
+          <button
+            className='btn btn-primary btn-sm mt-2'
+            onClick={this.submitHandler}
+          >
+            Submit
+          </button>
+
+          <hr />
+
+          <Child email={this.state.email} />
+        </div>
+      </>
+    );
   }
 }
 
